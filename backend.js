9079eed3bf3a408633e090f1d64789cfa48af6c5
yoknome <47323658+yoknome@users.noreply.github.com>
2019-08-27 17:18:21 +0200

    Upload stable Chatbor version
    - response if user hasn't entered a question
    - random response if user hast entered text
    - renamed files
    - added a background and main style to the site

---
+++ backend.js
@@ -0,0 +1,31 @@
+const httpMock = require('@Zemke/http-mock')(3333); //Florians Mock HTTP
+
+//httpMock.add('/api/tournament/', __dirname + '/mock/api_tournament.json');
+
+httpMock.add('/getAnswer', getAnswer);
+
+// Possible answers:
+var en = [
+	["Yes.", "Definitely.", "Indeed."], // positive answers
+	["No.", "Under no circumstances."], // negative answers
+	["Maybe.", "Ask again.", "You know the answer to that."] // neutral answers
+];
+/*/ Possible german answers:
+var de = [
+	["Ja.", "Definitiv.", "Auf jeden Fall.", "Wahrscheinlich."], // positive answers
+	["Nein.", "Unter keinen Umständen."], // negative answers
+	["Vielleicht.", "Frag nochmal.", "Du kennst die Antwort darauf."] // neutral answers
+];*/
+
+/* http://localhost:3333/getAnswer will provide a random 
+ * positive, negative or neutral answer */
+function getAnswer() {
+	var i = getRandomInt(3);
+	var j = getRandomInt(en[i].length);
+	var answer = en[i][j];
+	return answer;
+}
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
